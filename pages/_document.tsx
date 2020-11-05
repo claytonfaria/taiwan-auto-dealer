5189f6429b0d859762642561fa1ce13091439b4f
Clayton Faria <claytonfaria@gmail.com>
2020-11-05 21:05:28 +0800

    remove unecessary code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html lang="en">
